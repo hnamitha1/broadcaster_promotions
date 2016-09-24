956a97159170b9a295aedcf7a41cff9a2a87f4b9
5367288 <namitha.h@gmail.com>
2016-09-24 23:32:42 +0000

    price discount class finished

--- lib/price_discount.rb
+++ lib/price_discount.rb
@@ -4,7 +4,19 @@ class PriceDiscount
         @discount_percentage = discount_percentage.to_f
     end
     
+    def discount(_, total)
+        if total.to_f > threshold
+            discount_amount(total.to_f).round 2
+        else
+            0
+        end
+    end
+    
     private
     
     attr_accessor :threshold, :discount_percentage
+    
+    def discount_amount(total)
+        total * (discount_percentage / 100)
+    end
 end
--- spec/price_discount_spec.rb
+++ spec/price_discount_spec.rb
@@ -1,5 +1,17 @@
 require 'price_discount'
 
 describe PriceDiscount do
+    subject(:total_price) {described_class.new(30, 10)}
+    let(:delivery) { [double, double] }
+    
+    describe '#discount' do
+        it 'calculates the discount if above the threshold' do
+            expect(total_price.discount(delivery, 31)).to eq 3.1
+        end
+            
+        it 'returns zero if the total is below the threshold' do
+            expect(total_price.discount(delivery, 29)).to eq 0
+        end
+  end
     
 end
